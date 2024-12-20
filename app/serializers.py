da7120a7790176762451b5564e6a2cfb433f448a
Alex Grytsai <a.grytsai1987@gmail.com>
2024-12-20 16:01:41 +0200

    Created TargetUpdateSerializer

--- app/serializers.py
+++ app/serializers.py
@@ -1,5 +1,6 @@
 from django.contrib.auth.password_validation import validate_password
 from rest_framework import serializers
+from rest_framework.exceptions import ValidationError
 
 from app.models import CatModel, TargetModel
 
@@ -35,3 +36,23 @@ class TargetModelSerializer(serializers.ModelSerializer):
 class TargetListSerializer(TargetModelSerializer):
     class Meta(TargetModelSerializer.Meta):
         fields = ["id", "name", "country", "completed", "notes"]
+
+
+class TargetUpdateSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = TargetModel
+        fields = ["id", "notes", "completed"]
+
+    def validate_completed(self, value: bool) -> bool:
+        if value:
+            raise ValidationError("You cannot uncomplete a target.")
+        return value
+
+    def update(
+        self, instance: TargetModel, validated_data: dict
+    ) -> TargetModel:
+        if instance.completed:
+            raise ValidationError(
+                "You cannot update the notes because the target is complete."
+            )
+        return super().update(instance, validated_data)
