0029029f504ad3eadae3ae2b6a8b21de44be2a75
Alex Grytsai <a.grytsai1987@gmail.com>
2024-12-20 15:47:51 +0200

    Added to main urls paths for token and swager

--- SCA/urls.py
+++ SCA/urls.py
@@ -1,6 +1,36 @@
 from debug_toolbar.toolbar import debug_toolbar_urls
+from django.contrib.auth.views import LogoutView
 from django.urls import path, include
+from drf_spectacular.views import SpectacularAPIView, SpectacularSwaggerView
+from rest_framework_simplejwt.views import TokenObtainPairView, \
+    TokenRefreshView, TokenVerifyView
 
 urlpatterns = [
+path(
+        "api/v1/token/",
+        TokenObtainPairView.as_view(),
+        name="token_obtain_pair",
+    ),
+    path(
+        "api/v1/token/refresh/",
+        TokenRefreshView.as_view(),
+        name="token_refresh",
+    ),
+    path(
+        "api/v1/token/verify/",
+        TokenVerifyView.as_view(),
+        name="token_verify",
+    ),
+    path(
+        "api/v1/token/logout/",
+        LogoutView.as_view(),
+        name="token_logout",
+    ),
+    path("api/v1/schema/", SpectacularAPIView.as_view(), name="schema"),
+    path(
+        "api/v1/doc/swagger/",
+        SpectacularSwaggerView.as_view(url_name="schema"),
+        name="swagger-ui",
+    ),
     path("api/v1/", include("app.urls")),
 ] + debug_toolbar_urls()
