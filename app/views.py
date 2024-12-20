809c9fe341e080595850711cd4518bf7b6914462
Alex Grytsai <a.grytsai1987@gmail.com>
2024-12-20 16:05:21 +0200

    Created CatViewSet

--- app/views.py
+++ app/views.py
@@ -1,3 +1,21 @@
-from django.shortcuts import render
+from rest_framework import viewsets
+from rest_framework.permissions import IsAuthenticated, IsAdminUser
 
-# Create your views here.
+from app.models import CatModel
+from app.serializers import CatSerializer, CatUpdateSerializer
+
+
+class CatViewSet(viewsets.ModelViewSet):
+    queryset = CatModel.objects.all()
+    permission_classes = (IsAuthenticated,)
+    serializer_class = CatSerializer
+
+    def get_permissions(self):
+        if self.action in ("create", "update", "partial_update", "delete"):
+            self.permission_classes = (IsAdminUser,)
+        return super().get_permissions()
+
+    def get_serializer_class(self):
+        if self.action in ("update", "partial_update"):
+            self.serializer_class = CatUpdateSerializer
+        return super().get_serializer_class()
